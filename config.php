24f45e8cba6857edaa5b6ef51bfd1fb8c06c1a30
Ghildan Syarif Ayatullah <ghildansa@gmail.com>
2026-03-27 10:41:47 +0700

    setup deploy vercel

--- config.php
+++ config.php
@@ -1,14 +1,16 @@
 <?php
 
-$server = "localhost";
-$user = "root";
-$password = "";
-$database_name = "seminair";
+// Support local defaults and cloud env vars (e.g. Vercel).
+$server = getenv('DB_HOST') ?: "localhost";
+$user = getenv('DB_USER') ?: "root";
+$password = getenv('DB_PASSWORD') !== false ? getenv('DB_PASSWORD') : "";
+$database_name = getenv('DB_NAME') ?: "seminair";
+$db_port = (int) (getenv('DB_PORT') ?: 3306);
 
-$db = new mysqli($server, $user, $password, $database_name);
+$db = new mysqli($server, $user, $password, $database_name, $db_port);
 
-if (!$db) {
-    die("Gagal terhubung dengan database: " . mysqli_connect_error());
+if ($db->connect_error) {
+    die("Gagal terhubung dengan database: " . $db->connect_error);
 }
 
 
